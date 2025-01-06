3484f8721c26997c7c03601999a0625752ed9e6c
Kevin Bonilla <kb1909zzz@gmail.com>
2025-01-06 12:29:26 -0600

    :white_check_mark: User options in sidebar app

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -3,6 +3,7 @@ import { menuItems } from "./sidebar/menu-items";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -10,6 +11,13 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "./ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { ChevronUp, LogOut, User2 } from "lucide-react";
 
 export const AppSidebar = () => {
   return (
@@ -19,23 +27,50 @@ export const AppSidebar = () => {
           <SidebarGroupLabel>Momotus Evolv</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {
-                menuItems.map(({icon: Icon, title, url}) => (
-                  <SidebarMenuItem key={title}>
-                    <SidebarMenuButton asChild>
-                      <NavLink to={url}>
-                        <Icon />
-                        <span>{title}</span>
-
-                      </NavLink>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))
-              }
+              {menuItems.map(({ icon: Icon, title, url }) => (
+                <SidebarMenuItem key={title}>
+                  <SidebarMenuButton asChild>
+                    <NavLink to={url}>
+                      <Icon />
+                      <span>{title}</span>
+                    </NavLink>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> Kevin Bonilla
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span className="font-inter">Cuenta Momotus</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span className="font-inter">Soporte</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span className="font-inter text-red-600">Cerrar Sesión</span>
+                  <LogOut className="ml-auto text-red-600"/>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 };
